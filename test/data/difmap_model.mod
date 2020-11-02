3e5a199694964c05170a46dd637a5be6ef1a2fdc
Alexander Plavin <alexander@plav.in>
2020-11-02 16:22:59 +0300

    use local test files instead of downloading

---
+++ test/data/difmap_model.mod
@@ -0,0 +1,7 @@
+! Center RA: 23 59 35.49155,  Dec: -31 33 43.82464 (2000.0)
+! Tentative model.
+! Flux (Jy) Radius (mas)  Theta (deg)  Major FWHM (mas)  Axial ratio   Phi (deg) T \
+! Freq (Hz)     SpecIndex
+  0.649486v    0.159492v    -35.1567v     2.30367v     0.00000v   -46.7876  1
+-0.00937331v     39.6212v     29.0287v     6.04465v     1.00000     0.00000  1
+1.32054e+09v 9.78339e+09v     26.6947v     322699.v     1.00000     0.00000  1
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,36 +1,10 @@
 using Test
-using Pkg.Artifacts
 using DataFrames
 using Statistics
 using AxisKeys
 using Unitful
 using Dates
-
-
-import VLBIData
-const VLBI = VLBIData
-
-
-let
-    artifact_toml = joinpath(@__DIR__, "Artifacts.toml")
-    art_hash = artifact_hash("test_data", artifact_toml)
-    if art_hash === nothing || !artifact_exists(art_hash)
-        @info "Creating artifact"
-        art_hash_new = create_artifact() do artifact_dir
-            for url in [
-                    "http://astrogeo.org/images/J0000+0248/J0000+0248_C_2016_01_03_pet_map.fits",
-                    "http://astrogeo.org/images/J0000+0248/J0000+0248_C_2016_01_03_pet_vis.fits",
-                ]
-                download(url, joinpath(artifact_dir, basename(url)))
-            end
-        end
-        if art_hash === nothing
-            bind_artifact!(artifact_toml, "test_data", art_hash_new, force=true)
-        else
-            @assert art_hash == art_hash_new
-        end
-    end
-end
+import VLBIData; const VLBI = VLBIData
 
 
 @testset "utils" begin
@@ -51,14 +25,14 @@ end
 
 @testset "fits image" begin
     @testset "read nothing" begin
-        img = VLBI.load(VLBI.FitsImage, joinpath(artifact"test_data", "J0000+0248_C_2016_01_03_pet_map.fits"), read_data=false, read_clean=false)
+        img = VLBI.load(VLBI.FitsImage, "./data/map.fits", read_data=false, read_clean=false)
         @test img.clean_components === nothing
         @test img.data === nothing
         @test img.noise === nothing
     end
     
     @testset "read clean" begin
-        img = VLBI.load(VLBI.FitsImage, joinpath(artifact"test_data", "J0000+0248_C_2016_01_03_pet_map.fits"), read_data=false, read_clean=true)
+        img = VLBI.load(VLBI.FitsImage, "./data/map.fits", read_data=false, read_clean=true)
         @test img.data === nothing
         @test img.noise === nothing
         @test names(img.clean_components) == ["flux", "ra", "dec"]
@@ -70,7 +44,7 @@ end
     end
     
     @testset "read data" begin
-        img = VLBI.load(VLBI.FitsImage, joinpath(artifact"test_data", "J0000+0248_C_2016_01_03_pet_map.fits"), read_data=true, read_clean=false)
+        img = VLBI.load(VLBI.FitsImage, "./data/map.fits", read_data=true, read_clean=false)
         @test img.clean_components === nothing
         @test img.noise ≈ 9.5605465f-5
         @test size(img.data) == (512, 512)
@@ -82,14 +56,14 @@ end
     end
     
     @testset "read both" begin
-        img = VLBI.load(VLBI.FitsImage, joinpath(artifact"test_data", "J0000+0248_C_2016_01_03_pet_map.fits"), read_data=true, read_clean=true)
-        @test img.clean_components == VLBI.load(VLBI.FitsImage, joinpath(artifact"test_data", "J0000+0248_C_2016_01_03_pet_map.fits"), read_data=false, read_clean=true).clean_components
-        @test img.data == VLBI.load(VLBI.FitsImage, joinpath(artifact"test_data", "J0000+0248_C_2016_01_03_pet_map.fits"), read_data=true, read_clean=false).data
+        img = VLBI.load(VLBI.FitsImage, "./data/map.fits", read_data=true, read_clean=true)
+        @test img.clean_components == VLBI.load(VLBI.FitsImage, "./data/map.fits", read_data=false, read_clean=true).clean_components
+        @test img.data == VLBI.load(VLBI.FitsImage, "./data/map.fits", read_data=true, read_clean=false).data
     end
 end
 
 @testset "uvfits" begin
-    uv = VLBI.load(VLBI.UVData, joinpath(artifact"test_data", "J0000+0248_C_2016_01_03_pet_vis.fits"))
+    uv = VLBI.load(VLBI.UVData, "./data/vis.fits")
     @test uv.header.object == "J0000+0248"
     @test uv.header.date_obs === Date(2016, 1, 3)
     @test uv.header.stokes == [:RR]
