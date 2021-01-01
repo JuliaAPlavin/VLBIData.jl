77c795ce2f32575491d323f43e332689a3679ebe
Alexander Plavin <alexander@plav.in>
2021-01-01 00:00:00 +0000

    convert deg2rad in difmap models

--- src/difmap_files.jl
+++ src/difmap_files.jl
@@ -4,15 +4,17 @@ using DataFrames
 struct DifmapModel end
 
 function load(::Type{DifmapModel}, source)
-    df = CSV.read(source, DataFrame, comment="!", delim=" ", ignorerepeated=true, header=["flux", "radius", "theta", "major", "ratio", "phi", "T"])
+    df = CSV.read(source, DataFrame, comment="!", delim=" ", ignorerepeated=true, header=["flux", "radius", "theta_deg", "major", "ratio", "phi_deg", "T"])
     df = mapcols(df) do col
         map(col) do x
             ismissing(x) ? missing : parse(Float64, strip(string(x), 'v'))
         end
     end
     transform!(df,
-        [:radius, :theta] => ((r, t) -> r .* sind.(t)) => :ra,
-        [:radius, :theta] => ((r, t) -> r .* cosd.(t)) => :dec,
+        [:radius, :theta_deg] => ((r, t) -> r .* sind.(t)) => :ra,
+        [:radius, :theta_deg] => ((r, t) -> r .* cosd.(t)) => :dec,
+        :theta_deg => ByRow(deg2rad) => :theta,
+        :phi_deg => ByRow(deg2rad) => :phi,
     )
     return df
 end
