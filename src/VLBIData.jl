ff429cdb8ef7804e3bc16a4eaaeb7a6bfc5c2714
Alexander Plavin <alexander@plav.in>
2021-01-01 00:00:00 +0000

    fix and uncomment difmap_files

--- src/VLBIData.jl
+++ src/VLBIData.jl
@@ -3,6 +3,6 @@ module VLBIData
 include("fitsutils.jl")
 include("uvdata.jl")
 include("fitsimage.jl")
-# include("difmap_files.jl")
+include("difmap_files.jl")
 
 end
--- src/difmap_files.jl
+++ src/difmap_files.jl
@@ -1,12 +1,14 @@
 import CSV
+using DataFrames
 
 struct DifmapModel end
 
 function load(::Type{DifmapModel}, path)
     df = CSV.read(path, comment="!", delim=" ", ignorerepeated=true, header=["flux", "radius", "theta", "major", "ratio", "phi", "T", "freq", "specindex"])
     df = mapcols(c -> parse.(Float64, strip.(string.(c), 'v')), df)
-    df = df |>
-        @mutate(ra = _.radius * sin(_.theta |> deg2rad), dec = _.radius * cos(_.theta |> deg2rad)) |>
-        DataFrame
+    select!(df,
+        :theta => (x->sin(x |> deg2rad)) => :ra,
+        :theta => (x->cos(x |> deg2rad)) => :dec,
+    )
     return df
 end
