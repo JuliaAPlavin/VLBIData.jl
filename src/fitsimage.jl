fe2673827ede0f3afbc0e50f0bea2a1aec81db84
aplavin <aplavin@users.noreply.github.com>
2025-01-01 00:00:00 +0000

    Deploying to gh-pages from  @ 18b2647540c598a1fd5bb3cc60638fef473da9ea 🚀

--- src/fitsimage.jl
+++ src/fitsimage.jl
@@ -13,9 +13,12 @@ function load(::Type{FitsImage}, path; read_data=true)
         data = if read_data
             haskey(header, "BSCALE") && @assert header["BSCALE"] == 1
             haskey(header, "BZERO" ) && @assert header["BZERO" ] == 0
-            @assert header["BUNIT"] == "JY/BEAM"
+            header["BUNIT"] ∈ ("JY/BEAM", "JY/PIXEL") || error("Unknown BUNIT: $(header["BUNIT"])")
             data = read(primary)
-            data = dropdims(data, dims=(3, 4))
+            ndims(data) ∈ (2, 4) || error("Unexpected data shape: $(size(data))")
+            if ndims(data) == 4
+                data = dropdims(data, dims=(3, 4))
+            end
             KeyedArray(data; image_named_axiskeys(header)...)
         else
             nothing
@@ -30,28 +33,28 @@ AxisKeys.named_axiskeys(fi::FitsImage) = image_named_axiskeys(fi.header)
 image_axiskeys(fh::FITSHeader) = values(image_named_axiskeys(fh))
 function image_named_axiskeys(fh::FITSHeader)
     mul = 1u"°" |> u"mas"
-    ra  = axis_vals(fh, "RA---SIN", zero_reference=true) * mul
-    dec = axis_vals(fh, "DEC--SIN", zero_reference=true) * mul
+    ra  = axis_vals(fh, r"RA-+SIN", zero_reference=true) * mul
+    dec = axis_vals(fh, r"DEC-+SIN", zero_reference=true) * mul
     (; ra, dec)
 end
 
 function pixel_size(fi::FitsImage)
-    ra_step  = axis_dict(fi.header, "RA---SIN")["CDELT"] |> abs
-    dec_step = axis_dict(fi.header, "DEC--SIN")["CDELT"] |> abs
+    ra_step  = axis_dict(fi.header, r"RA-+SIN")["CDELT"] |> abs
+    dec_step = axis_dict(fi.header, r"DEC-+SIN")["CDELT"] |> abs
     @assert ra_step == dec_step
     return ra_step * u"°" .|> u"mas"
 end
 
 function pixel_steps(fi::FitsImage)
-    ra_step  = axis_dict(fi.header, "RA---SIN")["CDELT"]
-    dec_step = axis_dict(fi.header, "DEC--SIN")["CDELT"]
+    ra_step  = axis_dict(fi.header, r"RA-+SIN")["CDELT"]
+    dec_step = axis_dict(fi.header, r"DEC-+SIN")["CDELT"]
     @assert abs(ra_step) == abs(dec_step)
     return SVector(ra_step, dec_step) .* u"°" .|> u"mas"
 end
 
 function pixel_area(fi::FitsImage)
-    ra_step  = axis_dict(fi.header, "RA---SIN")["CDELT"] |> abs
-    dec_step = axis_dict(fi.header, "DEC--SIN")["CDELT"] |> abs
+    ra_step  = axis_dict(fi.header, r"RA-+SIN")["CDELT"] |> abs
+    dec_step = axis_dict(fi.header, r"DEC-+SIN")["CDELT"] |> abs
     return (ra_step * u"°" .|> u"mas") * (dec_step * u"°" .|> u"mas")
 end
 
--- src/fitsutils.jl
+++ src/fitsutils.jl
@@ -5,22 +5,27 @@ axis_types(fh::FITSHeader) = @p begin
     end
 end
 
-function axis_ind(fh::FITSHeader, ctype::String)
-    matching_cards = [k => v for (k, v) in pairs(fh) if v == ctype]
-    @assert length(matching_cards) == 1
-    key = matching_cards[1].first
+_ismatch(needle::AbstractString, haystack::AbstractString) = needle == haystack
+_ismatch(needle::Regex, haystack::AbstractString) = occursin(needle, haystack)
+_ismatch(needle, haystack) = false
+
+function axis_ind(fh::FITSHeader, ctype)
+    matching_card = @p pairs(fh) filteronly() do (k, v)
+        _ismatch(ctype, v)
+    end
+    key = matching_card.first
     m = match(r"^CTYPE(\d)$", key)
     return parse(Int, m[1])
 end
 
-function axis_dict(fh::FITSHeader, ctype::String)
+function axis_dict(fh::FITSHeader, ctype)
     ind = axis_ind(fh, ctype)
     re = Regex("^([A-Z]+)$(ind)\$")
     matching_cards = [match(re, k)[1] => v for (k, v) in pairs(fh) if occursin(re, k)]
     return Dict(matching_cards)
 end
 
-function axis_vals(fh::FITSHeader, ctype::String; zero_reference=false)
+function axis_vals(fh::FITSHeader, ctype; zero_reference=false)
     dict = axis_dict(fh, ctype)
     if zero_reference
         return ((1:dict["NAXIS"]) .- dict["CRPIX"]) .* dict["CDELT"]
@@ -29,7 +34,7 @@ function axis_vals(fh::FITSHeader, ctype::String; zero_reference=false)
     end
 end
     
-function axis_val(fh::FITSHeader, ctype::String; zero_reference=false)
+function axis_val(fh::FITSHeader, ctype; zero_reference=false)
     vals = axis_vals(fh, ctype, zero_reference=zero_reference)
     @assert length(vals) == 1
     return first(vals)
