6b151f64fb41d95d9aaa46657924ec7478c195dd
Alexander Plavin <alexander@plav.in>
2025-06-17 21:26:11 -0400

    keep constant columns in averaging

--- src/averaging.jl
+++ src/averaging.jl
@@ -4,18 +4,23 @@ aggspec(bl, specs::AbstractVector{<:VisSpec}) = VisSpec(bl, mean(x->UV(x), specs
 
 function average_data(uvtbl, strategy::AbstractScanStrategy; avgvals=U.weightedmean)
     uvtbl_with_scans = add_scan_ids(uvtbl, strategy)
-    intervals = scan_intervals(uvtbl_with_scans)
+    const_part = @p getproperties(uvtbl_with_scans) (@delete __[(:freq_spec, :stokes, :scan_id, :value, :spec)]) filter(allequal) map(uniqueonly)
+    return _average_data_by_scans(uvtbl_with_scans, const_part; avgvals)
+end
 
+function _average_data_by_scans(uvtbl_with_scans, const_part; avgvals)
+    intervals = scan_intervals(uvtbl_with_scans)
     @p begin
         uvtbl_with_scans
         groupview_vg((;bl=Baseline(_), _.freq_spec, _.stokes, _.scan_id))
         map((;
+            const_part...,
             delete(key(_), @o _.bl _.scan_id)...,
             count = length(_),
             value = avgvals(_.value),
             datetime = _mean(intervals[key(_).scan_id]),
             spec = aggspec(key(_).bl, _.spec),
-            scan_id = key(_).scan_id
+            scan_id = key(_).scan_id,
         ))
         StructArray()
     end
@@ -27,6 +32,11 @@ end
 
 function average_data(src_, strategy::FixedTimeIntervals; avgvals=U.weightedmean)
     src = StructArray(src_)
+    const_part = @p getproperties(src) (@delete __[(:freq_spec, :stokes, :value, :spec)]) filter(allequal) map(uniqueonly)
+    return _average_data_by_time(src, strategy, const_part; avgvals)
+end
+
+function _average_data_by_time(src, strategy::FixedTimeIntervals, const_part; avgvals)
     mindt = minimum(src.datetime)
     avg_interval = @p let
         float(strategy.interval)
@@ -39,11 +49,12 @@ function average_data(src_, strategy::FixedTimeIntervals; avgvals=U.weightedmean
             timestep = ((_.datetime - mindt) / avg_interval) |> upreferred |> x->trunc(Int, x),
         ))
         map((;
+            const_part...,
             delete(key(_), @o _.bl _.timestep)...,
             count = length(_),
             value = avgvals(_.value),
             datetime = mindt + (key(_).timestep + 0.5) * avg_interval,
-            spec = aggspec(key(_).bl, _.spec)
+            spec = aggspec(key(_).bl, _.spec),
         ))
         StructArray()
     end
