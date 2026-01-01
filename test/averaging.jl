13fa599dd8df04e10f9bdc63a880ce82b65d2c62
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    fix test

--- test/averaging.jl
+++ test/averaging.jl
@@ -14,19 +14,19 @@
     ]
 
     @test VLBI.average_data(VLBI.FixedTimeIntervals(0u"minute"), uvtbl_orig)::StructArray == [
-        (source="SgrA*", freq_spec=123, stokes=:RR, count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 0, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 1))),
-        (source="SgrA*", freq_spec=123, stokes=:RR, count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 0, 10), spec=VisSpec(Baseline((1, 2)), UV(0, 2))),
-        (source="SgrA*", freq_spec=123, stokes=:RR, count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 0, 20), spec=VisSpec(Baseline((1, 3)), UV(0, 3))),
-        (source="SgrA*", freq_spec=123, stokes=:RR, count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 20, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 4))),
-        (source="SgrA*", freq_spec=123, stokes=:LL, count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 20, 10), spec=VisSpec(Baseline((1, 2)), UV(0, 5))),
-        (source="SgrA*", freq_spec=124, stokes=:LL, count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 20, 10), spec=VisSpec(Baseline((1, 2)), UV(0, 5))),
+        (freq_spec=123, stokes=:RR, source="SgrA*", count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 0, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 1))),
+        (freq_spec=123, stokes=:RR, source="SgrA*", count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 0, 10), spec=VisSpec(Baseline((1, 2)), UV(0, 2))),
+        (freq_spec=123, stokes=:RR, source="SgrA*", count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 0, 20), spec=VisSpec(Baseline((1, 3)), UV(0, 3))),
+        (freq_spec=123, stokes=:RR, source="SgrA*", count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 20, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 4))),
+        (freq_spec=123, stokes=:LL, source="SgrA*", count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 20, 10), spec=VisSpec(Baseline((1, 2)), UV(0, 5))),
+        (freq_spec=124, stokes=:LL, source="SgrA*", count=1, value=1±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 20, 10), spec=VisSpec(Baseline((1, 2)), UV(0, 5))),
     ]
     @test VLBI.average_data(VLBI.FixedTimeIntervals(10u"minute"), uvtbl_orig)::StructArray == [
-        (source="SgrA*", freq_spec=123, stokes=:RR, count=2, value=1±ᵤ0.07071067811865475, datetime=DateTime(2020, 1, 1, 0, 5, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 1.5))),
-        (source="SgrA*", freq_spec=123, stokes=:RR, count=1, value=1.0±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 5, 0), spec=VisSpec(Baseline((1, 3)), UV(0, 3.0))),
-        (source="SgrA*", freq_spec=123, stokes=:RR, count=1, value=1.0±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 25, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 4.0))),
-        (source="SgrA*", freq_spec=123, stokes=:LL, count=1, value=1.0±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 25, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 5.0))),
-        (source="SgrA*", freq_spec=124, stokes=:LL, count=1, value=1.0±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 25, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 5.0))),
+        (freq_spec=123, stokes=:RR, source="SgrA*", count=2, value=1±ᵤ0.07071067811865475, datetime=DateTime(2020, 1, 1, 0, 5, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 1.5))),
+        (freq_spec=123, stokes=:RR, source="SgrA*", count=1, value=1.0±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 5, 0), spec=VisSpec(Baseline((1, 3)), UV(0, 3.0))),
+        (freq_spec=123, stokes=:RR, source="SgrA*", count=1, value=1.0±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 25, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 4.0))),
+        (freq_spec=123, stokes=:LL, source="SgrA*", count=1, value=1.0±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 25, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 5.0))),
+        (freq_spec=124, stokes=:LL, source="SgrA*", count=1, value=1.0±ᵤ0.1, datetime=DateTime(2020, 1, 1, 0, 25, 0), spec=VisSpec(Baseline((1, 2)), UV(0, 5.0))),
     ]
 end
 
