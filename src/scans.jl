ff67a741bb812b109ab42e821dae1fdd272510b4
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    swap argument order in some functions

--- src/scans.jl
+++ src/scans.jl
@@ -4,7 +4,7 @@ abstract type AbstractScanStrategy end
     min_gap = 1u"minute"
 end
 
-function scan_ids(uvtbl::StructVector, strategy::GapBasedScans)
+function scan_ids(strategy::GapBasedScans, uvtbl::StructVector)
     @modify(uvtbl |> sort(_, by=x->x.datetime)) do uvtbl
         scan_id_ref = Ref(1)
         prev_dt_ref = Ref(uvtbl.datetime[1])
@@ -20,19 +20,20 @@ function scan_ids(uvtbl::StructVector, strategy::GapBasedScans)
     end
 end
 
-function add_scan_ids(uvtbl, strategy::AbstractScanStrategy)
+function add_scan_ids(strategy::AbstractScanStrategy, uvtbl)
     uvtbl = StructArray(uvtbl)
     @assert !hasproperty(uvtbl, :scan_id)
-    @insert uvtbl.scan_id = scan_ids(uvtbl, strategy)
+    @insert uvtbl.scan_id = scan_ids(strategy, uvtbl)
 end
 
-function scan_intervals(uvtbl, strategy::Union{Nothing,AbstractScanStrategy}=nothing)
+scan_intervals(uvtbl) = scan_intervals(nothing, uvtbl)
+function scan_intervals(strategy::Union{Nothing,AbstractScanStrategy}, uvtbl)
     uvtbl = StructArray(uvtbl)
     uvtbl = if hasproperty(uvtbl, :scan_id)
         @p uvtbl sort(by=_.scan_id)
     else
         isnothing(strategy) && error("No scan strategy provided and no scan_id present in the table.")
-        add_scan_ids(uvtbl, strategy)
+        add_scan_ids(strategy, uvtbl)
     end
     @p let
         uvtbl
