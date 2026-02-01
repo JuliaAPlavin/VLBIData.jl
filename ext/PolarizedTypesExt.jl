276907c9f35dd4ca7b9fd1686b0ab192b1b83fcf
Alexander Plavin <alexander@plav.in>
2026-02-01 00:00:00 +0000

    IPol in uvtable_values_to function

--- ext/PolarizedTypesExt.jl
+++ ext/PolarizedTypesExt.jl
@@ -24,4 +24,18 @@ function VLBIData.uvtable_values_to(::Type{PolT.CoherencyMatrix}, uvtbl)
 	end
 end
 
+function VLBIData.uvtable_values_to(::Type{PolT.IPol}, uvtbl)
+	grs = @p uvtbl group_vg((;_.datetime, _.freq_spec, _.spec))
+	return map(grs) do gr
+		par_hands = @p gr filter(x -> VLBIData.is_parallel_hands(x.stokes))
+		length(par_hands) == 2 || error("expected 2 parallel-hand Stokes per group, got $(length(par_hands))")
+		val = sum(x -> x.value, par_hands) / 2
+		@p let
+			first(gr)
+			@set __.value = val
+			@delete __.stokes
+		end
+	end
+end
+
 end
