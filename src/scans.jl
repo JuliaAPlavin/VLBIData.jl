114ff083b886b84791315fbfdf95180c386c5d5e
Alexander Plavin <alexander@plav.in>
2026-03-01 00:00:00 +0000

    scan-related type stability improvements

--- src/scans.jl
+++ src/scans.jl
@@ -5,7 +5,7 @@ abstract type AbstractScanStrategy end
 end
 
 @stable function scan_ids(strategy::GapBasedScans, uvtbl::StructVector)
-    min_gap = strategy.min_gap
+    min_gap::typeof(1.0u"s") = strategy.min_gap
     scan_id_ref = Ref(1)
     prev_dt_ref = Ref(minimum(uvtbl.datetime))
     @modify($(uvtbl.datetime) |> sort) do datetimes
@@ -20,19 +20,26 @@ end
     end
 end
 
-@stable function add_scan_ids(strategy::AbstractScanStrategy, uvtbl)
+function add_scan_ids(strategy::AbstractScanStrategy, uvtbl)
     uvtbl = StructArray(uvtbl)
     @assert !hasproperty(uvtbl, :scan_id)
     @insert uvtbl.scan_id = scan_ids(strategy, uvtbl)
 end
 
 @stable scan_intervals(uvtbl) = scan_intervals(nothing, uvtbl)
-@stable function scan_intervals(strategy::Union{Nothing,AbstractScanStrategy}, uvtbl)
-    uvtbl = StructArray(uvtbl)
-    if !hasproperty(uvtbl, :scan_id)
-        isnothing(strategy) && error("No scan strategy provided and no scan_id present in the table.")
-        uvtbl = add_scan_ids(strategy, uvtbl)
+scan_intervals(strategy, uvtbl) = scan_intervals(strategy, StructArray(uvtbl))
+@stable function scan_intervals(strategy::Nothing, uvtbl::StructArray)
+    hasproperty(uvtbl, :scan_id) || error("No scan strategy provided and no scan_id present in the table.")
+    @p let
+        uvtbl
+        groupview(_.scan_id; restype=Vector)
+        map() do gr
+            Interval(extrema(gr.datetime)...)
+        end
     end
+end
+@stable function scan_intervals(strategy::AbstractScanStrategy, uvtbl::StructArray)
+    uvtbl = add_scan_ids(strategy, uvtbl)
     @p let
         uvtbl
         groupview(_.scan_id; restype=Vector)
